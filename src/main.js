04a7b805d48d9f9468d183939c2f134660bfd39b
Steven Rollason <2099542+gadgetchnnel@users.noreply.github.com>
2022-04-03 16:38:51 +0100

    Tippy styling fixes

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { getLovelace } from './helpers.js';
 import tippy from 'tippy.js';
 import tippyStyle from 'tippy.js/dist/tippy.css';
 import tippyAnimationStyle from 'tippy.js/animations/scale-extreme.css';
+import tippyThemeStyle from './tippy-theme.css';
 
 const CUSTOM_TYPE_PREFIX = "custom:";
 
@@ -33,11 +34,12 @@ class HeaderCards {
  	}
  	
  	tippyStyles() {
-    return
-      css`
-        ${unsafeCSS(tippyStyle)}
-      	${unsafeCSS(tippyAnimationStyle)}
-      `;
+    	let styles = `
+    		${tippyStyle}
+    		${tippyAnimationStyle}
+    		${tippyThemeStyle}
+    	`;
+    	return styles;
   	}
  	
  	addCardsHandler(){
@@ -120,6 +122,7 @@ class HeaderCards {
 	addCard(cardConfig, element) {
 		let tag = cardConfig.type;
 		let card = this.createCardElement(cardConfig);
+		card.classList.add("header-card");
 		card.style.display = "inline-block";
 		card.hass = this.hass;
 		card.style["flex"] = "1";
@@ -136,20 +139,26 @@ class HeaderCards {
     	
 		if(tooltipCardConfig){
 			let tooltipCard = this.createCardElement(tooltipCardConfig);
+			tooltipCard.classList.add("header-tooltip-card");
 			tooltipCard.style.setProperty("width","2000px", "important");
 			tooltipCard.hass = this.hass;
+			let that = this;
 			tippy(div, {
   					content: tooltipCard,
 	 				arrow: true,
   					animation: 'scale-extreme',
   					placement: 'auto',
+  					theme: 'homeassistant',
   					appendTo: div,
   					trigger: 'click',
   					hideOnClick: true,
   					maxWidth: 'none',
-  					onShow(instance) {
-    					console.log("Instance", instance);
-    					return true;	
+  					onShown(instance) {
+  						let thisCard = instance.reference.querySelector(".header-tooltip-card");
+  						if(thisCard) thisCard.hass = that.hass;
+  						let haCard = thisCard && thisCard.shadowRoot;
+ 			        		haCard = haCard && haCard.querySelector("ha-card");
+ 						if(haCard) haCard.style.setProperty("box-shadow", "none", "important");	
     				},
     				popperOptions: {
     					modifiers: [
@@ -176,7 +185,6 @@ class HeaderCards {
 				element.style["pointer-events"] = "auto";
 				div.style["pointer-events"] = "auto";
 				card.style["pointer-events"] = "none";
-				
 		}
 		else{    
 			element.appendChild(card);
@@ -198,6 +206,7 @@ class HeaderCards {
 	
 	addBadge(badgeConfig, element) {
 		let badge = this.createBadgeElement(badgeConfig);
+		badge.classList.add("header-badge");
 		badge.hass = this.hass;
 		badge.style.setProperty("--ha-label-badge-size", "2em");
 		badge.style.setProperty("--ha-label-badge-title-font-size", "0.6em");
@@ -208,20 +217,12 @@ class HeaderCards {
 	
 	applyHass(){
 		if(this.hass && this.toolbar){
-			let cards = this.toolbar.querySelector("#headerCards");
-			let badges = this.toolbar.querySelector("#headerBadges");
-			if(cards){
-					let children = [...cards.children];
-					children.forEach((div) => {
-					div.firstChild.hass = this.hass;
-				});
-			}
+			let items = this.toolbar.querySelectorAll("#headerCards div .header-card,.header-tooltip-card,.header-badge");
 			
-			if(badges){
-					let children = [...badges.children];
-					children.forEach((badge) => {
-					badge.hass = this.hass;
-				});
+			if(items){
+				items.forEach(item => {
+						item.hass = this.hass;
+					});
 			}
 		}
 	}
@@ -251,9 +252,10 @@ class HeaderCards {
     		
     		if(cards.length > 0 || badges.length > 0){
     			let outerDiv =  document.createElement("div");
+    			//console.log("Styles", this.tippyStyles());
     			
     			let style = document.createElement("style");
-    			style.appendChild(document.createTextNode(`.tippy-box[data-animation=fade][data-state=hidden]{opacity:0}[data-tippy-root]{max-width:calc(100vw - 10px)}.tippy-box{position:relative;background-color:#333;color:#fff;border-radius:4px;font-size:14px;line-height:1.4;white-space:normal;outline:0;transition-property:transform,visibility,opacity}.tippy-box[data-placement^=top]>.tippy-arrow{bottom:0}.tippy-box[data-placement^=top]>.tippy-arrow:before{bottom:-7px;left:0;border-width:8px 8px 0;border-top-color:initial;transform-origin:center top}.tippy-box[data-placement^=bottom]>.tippy-arrow{top:0}.tippy-box[data-placement^=bottom]>.tippy-arrow:before{top:-7px;left:0;border-width:0 8px 8px;border-bottom-color:initial;transform-origin:center bottom}.tippy-box[data-placement^=left]>.tippy-arrow{right:0}.tippy-box[data-placement^=left]>.tippy-arrow:before{border-width:8px 0 8px 8px;border-left-color:initial;right:-7px;transform-origin:center left}.tippy-box[data-placement^=right]>.tippy-arrow{left:0}.tippy-box[data-placement^=right]>.tippy-arrow:before{left:-7px;border-width:8px 8px 8px 0;border-right-color:initial;transform-origin:center right}.tippy-box[data-inertia][data-state=visible]{transition-timing-function:cubic-bezier(.54,1.5,.38,1.11)}.tippy-arrow{width:16px;height:16px;color:#333}.tippy-arrow:before{content:"";position:absolute;border-color:transparent;border-style:solid}.tippy-content{position:relative;padding:5px 9px;z-index:1}`));
+    			style.appendChild(document.createTextNode(this.tippyStyles()));
     			outerDiv.appendChild(style);
     			outerDiv.id = "headerCards";
     			outerDiv.style.display = "flex";
